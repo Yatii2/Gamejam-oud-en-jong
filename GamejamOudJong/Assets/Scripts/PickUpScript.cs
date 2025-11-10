3ff911cbac81af104994fed50162de254a45b698
EvertVanGroningen <145978161+EvertVanGroningen@users.noreply.github.com>
2025-11-10 16:06:12 +0100

    CleanupPickup

--- GamejamOudJong/Assets/Scripts/PickUpScript.cs
+++ GamejamOudJong/Assets/Scripts/PickUpScript.cs
@@ -3,17 +3,6 @@ using UnityEngine;
 public class PickUpScript : MonoBehaviour
 {
     public int score = 0;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnCollisionEnter2D(Collision2D other)
     {
