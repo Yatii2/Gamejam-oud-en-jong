0ddb1cb4bf73433c63a8b8444f0d005c8aee21dc
JosHub911 <166252789+JosHub911@users.noreply.github.com>
2025-11-11 21:38:34 +0100

    Added more sprites

---
+++ GamejamOudJong/Assets/Sprites/DetailScripts/Vertical Reveal.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VerticalReveal : MonoBehaviour
+{
+    public float duration = 1f;
+
+    RectTransform rect;
+    float timer = 0f;
+
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+
+        // Start fully collapsed vertically
+        rect.localScale = new Vector3(1, 0, 1);
+    }
+
+    void Update()
+    {
+        if (timer < duration)
+        {
+            timer += Time.deltaTime;
+            float t = Mathf.Clamp01(timer / duration);
+
+            // Scale Y from 0 → 1
+            rect.localScale = new Vector3(1, t, 1);
+        }
+    }
+}
