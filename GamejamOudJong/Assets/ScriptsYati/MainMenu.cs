690f1a87ffcf62223cc2368ab28a03c2f2997ef0
Yatii2 <146432678+Yatii2@users.noreply.github.com>
2025-11-12 16:08:22 +0100

    main menu + wallhack button sprite

---
+++ GamejamOudJong/Assets/ScriptsYati/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Blockout");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
+
