070b73d78980956766381cdda92896c5339f9616
EvertVanGroningen <145978161+EvertVanGroningen@users.noreply.github.com>
2025-11-12 11:22:06 +0100

    DifferenciateTags

--- GamejamOudJong/Assets/Scripts/PickUpScript.cs
+++ GamejamOudJong/Assets/Scripts/PickUpScript.cs
@@ -1,16 +1,31 @@
+using System;
 using UnityEngine;
 
 public class PickUpScript : MonoBehaviour
 {
     public int score = 0;
+    [SerializeField] public int winningScore;
 
     public void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("OnCollisionEnter");
-        if (other.gameObject.CompareTag("Pickup"))
+        if (other.gameObject.CompareTag("Pickup") && gameObject.CompareTag("Child"))
         {
             score++;
             Destroy(other.gameObject);
         }
+
+        if (other.gameObject.CompareTag("Child"))
+        {
+            //trigger hier de old man wins screen
+        }
+    }
+
+    private void Update()
+    {
+        if (score >= winningScore)
+        {
+            //trigger hier de child wins script
+        } 
     }
 }
